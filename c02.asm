9ebe0b6308b6251bd546c497c77a66be902d0b52
safi-io <m.safi.ullah@outlook.com>
2025-02-12 15:33:00 +0500

    Define Words

---
+++ c02.asm
@@ -0,0 +1,18 @@
+[org 0x0100]
+
+
+mov ax, [num1]
+mov bx, [num2]
+mov cx, [num3]
+
+add ax, bx
+add ax, cx
+mov [num4], ax
+
+mov ax, 0x4c00
+int 0x21
+
+num1: dw 5
+num2: dw 10
+num3: dw 15
+num4: dw 0
