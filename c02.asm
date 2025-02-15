b2f5187dde0f31ea125efd05dffd8821f2a9e457
safi-io <m.safi.ullah@outlook.com>
2025-02-15 09:56:11 +0500

    Playing with addresses

--- c02.asm
+++ c02.asm
@@ -1,18 +1,22 @@
 [org 0x0100]
 
+; program to add 3 numbers
+; using memory variables
+
+; mov [num1], [num2] illegal 
 
 mov ax, [num1]
-mov bx, [num2]
-mov cx, [num3]
+mov bx, [num1 + 2]
+mov cx, [num1 + 4]
 
 add ax, bx
 add ax, cx
-mov [num4], ax
+mov [num1 + 6], ax
 
 mov ax, 0x4c00
 int 0x21
 
 num1: dw 5
-num2: dw 10
+dw 10
 num3: dw 15
 num4: dw 0
